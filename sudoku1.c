c6d7366a4b4e2cb5f3351cc17a4f190a8d932cc2
MatrixMike <matroxmike@icloud.com>
2016-10-27 13:00:26 +1100

    cpp updates

--- sudoku1.c
+++ sudoku1.c
@@ -8,31 +8,29 @@
  * see www.cprogramming.com
  */
 
-
 #include <stdio.h>
 #include <stdbool.h>
 
 int main(int argc, char **argv)
 {
-	int zone1;   //  = 0x01d3;
+	int zone1;		//  = 0x01d3;
 	int zone2;
 	int zone3;
 	int mask;
 	mask = 0x0f;
-	
-	zone1 = 0b0111010011;
-	zone2 = 0b100;			// 4
-	zone2 = ~zone2;			// compliment {NOT} 
+
+	zone1 = 0 b0111010011;
+	zone2 = 0 b100;		// 4
+	zone2 = ~zone2;		// compliment {NOT} 
 	zone3 = zone1 | zone2;	// OR
 	zone3 = zone1 & zone2;	// AND
 	zone3 = zone1 ^ zone2;	// XOR
-	zone3 = zone1 ^ zone2;	// XOR	
-	zone3 = (zone1 & mask)| 0b01000000;	// mask
-	
-//	bool keep_going ;
-//	keep_going = true;
-//	keep_going = false;
-	printf(" test value : %d: %d: %d\n", zone1,zone2,zone3);
+	zone3 = zone1 ^ zone2;	// XOR  
+	zone3 = (zone1 & mask) | 0 b01000000;	// mask
+
+//      bool keep_going ;
+//      keep_going = true;
+//      keep_going = false;
+	printf(" test value : %d: %d: %d\n", zone1, zone2, zone3);
 	return 0;
 }
-
