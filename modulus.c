53c53d67ec4e1ba2109eace8df0016e24447e65a
Mike on Evo N410c <matroxmike@icloud.com>
2017-05-20 14:56:56 +1000

    after lint

--- modulus.c
+++ modulus.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 // 12 jun  2015
-
+// 20.05.2017 14:50:57
+// changes after running cppcheck (lint)
 // testing modulus arithmetic 
 
 int main ()  {
-	int i,r,m;
+//	int i,r,m;
 
-	for (i=0; i<10; i++) {
+	for (int i=0; i<10; i++) {
+		int r,m;
 		r = i / 3;
 		m = i % 3;
 		printf(" i=  %d  %d   %d   \n", i , r, m);
