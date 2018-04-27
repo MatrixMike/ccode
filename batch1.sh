44141434e14c6f335da2b22b9ddea5db15374981
Mike Hewitt <matroxmike@icloud.com>
2018-04-28 00:02:24 +1000

    tcc and gcc on i686

--- batch1.sh
+++ batch1.sh
@@ -1,8 +1,15 @@
-  #!/bin/bash
-for file in { vowel1 sudoku1 times11 diagonal facebook1 test }
+#!/bin/bash
+# Fri Apr 27 23:49:28 AEST 2018 
+for file in { vowel1 sudoku1 times11 diagonal facebook1 test modulus helloworld including }
 do
   echo $file.c
-  gcc $file.c  -o $file
+  rm $file
+# -std=99 for 
+# -lm for diagonal
+  gcc $file.c  -o $file -std=c99 -lm
 done
+tcc including.c  -vv -run -trigraphs
+gcc including.c  -o including   -MMD  -MF test1.txt
+gcc including.c  -o including   -MMD  -MF test1.txt    -H
 
 
