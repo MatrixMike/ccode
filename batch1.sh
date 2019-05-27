08890df7aa438e5f3a56d03fa865a1938b4f18ea
Mike H <matroxmike@icloud.com>
2019-05-28 09:58:40 +1000

    de-activated some lines

--- batch1.sh
+++ batch1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Fri Apr 27 23:49:28 AEST 2018 
-for file in diagonal facebook1 helloworld including modulus sudoku1 test times11 vowel1 
+for file in diagonal facebook1 helloworld modulus sudoku1 test times11 vowel1 fgetsTest strcpy1 times11
 do
   echo $file.c
   rm $file
@@ -8,8 +8,8 @@ do
 # -lm for diagonal
   gcc $file.c  -o $file -std=c99 -lm
 done
-tcc including.c  -vv -run -trigraphs
-gcc including.c  -o including   -MMD  -MF test1.txt
-gcc including.c  -o including   -MMD  -MF test1.txt    -H
+# tcc including.c  -vv -run -trigraphs
+#gcc including.c  -o including   -MMD  -MF test1.txt
+#gcc including.c  -o including   -MMD  -MF test1.txt    -H
 
 
