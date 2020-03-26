874b7b4e50a7f4dc98cecded078918a33f52ce80
Mike <matroxmike@icloud.com>
2020-03-26 14:15:50 +1100

    two logic methods

--- logicAnd.c
+++ logicAnd.c
@@ -13,16 +13,38 @@
 
 
 #include <stdio.h>
-
-int
-main (int argc, char **argv)
+void
+NotEq1 ()
 {
+  printf ("NotEq1\n");
   for (int i = 1; i < 20; i++)
     {
-      if ((i != 6) & (i != 10))
+      if ((i != 6) && (i != 10))
 	{
 	  printf ("value %2d \n", i);
 	}
     }
-  return 0;
+//  return 0;
+}
+
+void
+NotEq2 ()
+{				// not (A or B) = not A and not B
+  printf ("NotEq2\n");
+  for (int i = 1; i < 20; i++)
+    {
+      if (!(i == 6 || i == 10))
+	{
+	  printf ("value %2d \n", i);
+	}
+    }
+//  return 0;
+}
+
+int
+main (int argc, char **argv)
+{
+  NotEq1 ();
+  NotEq2 ();
+
 }
